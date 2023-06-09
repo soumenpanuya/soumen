fe20f8bbbc1c4b84df848d045b7383a07e0550db
soumenpanuya <63386446+soumenpanuya@users.noreply.github.com>
2023-06-09 23:28:55 +0530

    Update ja.js

--- ja.js
+++ ja.js
@@ -4,7 +4,5 @@ const search = document.querySelectorAll(".btn")[1];
 const totalcount = document.getElementById("to-count");
 const completecount = document.getElementById("co-count");
 add.onclick = (e) => {
-  if (e.target.textContent == "add") {
-    
-  completecount.textContent=input.value;
-}};
+  completecount.textContent='hello world';
+};
